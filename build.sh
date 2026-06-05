b6100f08e8194fae5fd63ad7b1b5b5c703da57ca
DautoSaldanha <dautosaldanhaalves28@gmail.com>
2026-06-05 16:09:08 -0300

    Antes do deploy

--- build.sh
+++ build.sh
@@ -1,13 +1,4 @@
-# build.sh
 echo "Starting build script"
-
 pip install -r requirements.txt
-
-# make migrations
-python3 manage.py makemigrations
-python3 manage.py migrate
-
-# collectstatic
-python3 manage.py collectstatic
-
+python3 manage.py collectstatic --noinput
 echo "Build script completed"
