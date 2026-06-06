a9b6f0909eb386ba193f2cc9cffedb3867cf4794
DautoSaldanha <dautosaldanhaalves28@gmail.com>
2026-06-06 12:22:51 -0300

    arrumando logica

--- app_tela/views.py
+++ app_tela/views.py
@@ -3,4 +3,17 @@ from django.contrib.auth.models import User
 
 
 def home(request):
-    return render(request, 'index.html')
+    usuarios = User.objects.all()
+    if request.method == 'POST':
+        nome = request.POST.get('name')
+        email = request.POST.get('email')
+        user = User.objects.filter(username=nome, email=email).first()
+        if not user:
+            user = User(username=nome, email=email)
+            user.save()
+            return render(request, 'index.html', {'usuarios': usuarios})
+        else:
+            usuario_existente = "Usuário já existe!"
+            return render(request, 'index.html', {'usuarios': usuarios, 'usuario_existente': usuario_existente})
+
+    return render(request, 'index.html', {'usuarios': usuarios})
